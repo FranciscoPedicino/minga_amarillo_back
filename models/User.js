fd5dad31863f3ba98e59f95d329d00507a448926
FranciscoPedicino <franciscopedi91@gmail.com>
2023-04-21 18:51:28 -0300

    b-06

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 let schema = new mongoose.Schema({
     email:{type:String ,required:true},
-    passeword:{type:String ,required:true},
+    password:{type:String ,required:true},
     photo:{type:String ,required:true},
     role:{type:Number ,required:true},
     is_online:{type:Boolean ,required:true},
@@ -10,8 +10,8 @@ let schema = new mongoose.Schema({
 
 },
 
-{timestamps: true }
+{timestamps:true}
 )
 let collection = 'users' //debe ser siempre en plural porque es un conjunto de recursos/datos/documentos
-let user=mongoose.model(schema,collection)
+let user= mongoose.model( collection ,schema)
 export default user 
